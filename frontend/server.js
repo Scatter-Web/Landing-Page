c10ab0ffa4b7bf5c7e6059d8378887424a291f8a
Kikols <francisco.lapamsilva@gmail.com>
2026-05-03 01:33:03 +0100

    fix docker finally

--- frontend/server.js
+++ frontend/server.js
@@ -8,7 +8,7 @@ const port = process.env.APP_PORT || 8080;
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Fallback to index.html for client-side routing
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
