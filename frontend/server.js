ece07e77f4fa798ffa0fa897b5cd21f01546e479
Kikols <francisco.lapamsilva@gmail.com>
2026-05-03 02:14:17 +0100

    try to fix docker

--- frontend/server.js
+++ frontend/server.js
@@ -12,6 +12,6 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
